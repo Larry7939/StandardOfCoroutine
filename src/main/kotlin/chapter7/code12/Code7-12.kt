7b2f23dfbcef9fbf0b2ad4aefa7abbc942e6f21f
Larry7939 <70442964+Larry7939@users.noreply.github.com>
2024-05-03 20:12:33 +0900

    Update Code7-12.kt

--- src/main/kotlin/chapter7/code12/Code7-12.kt
+++ src/main/kotlin/chapter7/code12/Code7-12.kt
@@ -8,7 +8,7 @@ fun main() {
     delay(100L) // 100밀리초 대기
     println("[${Thread.currentThread().name}] 코루틴 실행 완료")
   }
-  Thread.sleep(1000L)
+  Thread.sleep(1000L) // launch는 runBlocking과는 달리 호출된 스레드를 블로킹하지 않기때문에, 강제로 스레드를 Block하였음.
 }
 /*
 // 결과:
